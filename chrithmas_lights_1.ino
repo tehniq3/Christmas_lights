e0ec913560a2616a5473abad84507090751aec20
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2016-12-10 22:51:24 +0300

    Update and rename .gitignore to chrithmas_lights_1.ino

---
+++ chrithmas_lights_1.ino
@@ -0,0 +1,15 @@
+// original from http://www.makeuseof.com/tag/arduino-project-flashy-christmas-lights-ornaments/
+// changed by Nicu FLORICA (niq_ro) - http://www.tehnic.go.ro
+// & http://www.arduinotehniq.com
+
+void setup(){
+   for(int i=4; i<14; i++){  // each led is in series with 220-470 ohms resitor
+       pinMode(i,OUTPUT);
+   }
+}
+
+void loop(){
+  digitalWrite(random(4,14),HIGH);
+  delay(50);
+  digitalWrite(random(4,14),LOW);
+}
